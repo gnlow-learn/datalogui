199c02e842efab1fb4aa809de8f7e136d25b439c
Gnlow <gnlow@pm.me>
2025-09-14 20:56:51 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -5,3 +5,40 @@ const Human = datalog.newTable({
 })
 
 Human.assert({ name: "Alice" })
+
+const Pass = datalog.newTable({
+    dYear: datalog.NumberType,
+})
+const CurrentYear = datalog.newTable({
+    year: datalog.NumberType,
+})
+
+const CurrentYearQuery = datalog.query
+<{ year: number, dYear: number }>
+(({ year, dYear }) => {
+    Pass({ dYear })
+    CurrentYear({ year })
+})
+
+CurrentYearQuery.viewExt()
+    .mapEffect(({ kind, datum: { year, dYear }}) => {
+        if (kind == datalog.Added) {
+            Pass.retract({ dYear })
+            CurrentYear.retract({ year })
+            CurrentYear.assert({ year: year + dYear })
+        }
+    })
+    .onChange(() => {
+        CurrentYearQuery.runQuery()
+    })
+
+CurrentYearQuery.onDependencyChange(() => {
+    CurrentYearQuery.runQuery()
+})
+
+CurrentYear.view().onChange(() => {
+    console.log(CurrentYear.view().readAllData())
+})
+
+CurrentYear.assert({ year: 10 })
+Pass.assert({ dYear: 1 })
